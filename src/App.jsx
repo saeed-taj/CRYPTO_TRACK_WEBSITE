5bf81e601d38e3869b0d388f2c9d46d4c24e16af
wajahat taj <p233023@pwr.nu.edu.pk>
2025-04-05 20:37:24 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react' 
+import Navbar from './components/Navbar/Navbar.jsx'
+
+const App = () => {
+  return (
+    <div className = 'App'>
+       
+     <Navbar />
+
+      <h1>Hello World</h1>
+      <p>This is a simple React application.</p>
+    
+    </div>
+  );
+}
+export default App;
